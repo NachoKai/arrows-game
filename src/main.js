3ca597c256b1c60d87410ceaef7a39ed22486035
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2020-02-01 12:04:49 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,22 +7,42 @@ let keys = {
     arrowRight: 39
 }
 
+let keyUp = false
+let keyDown = false
+let keyLeft = false
+let keyRight = false
+
 document.addEventListener("keydown", e => {
     switch (e.keyCode) {
         case keys.arrowUp:
+            keyUp = true
+            keyDown = false
+            keyLeft = false
+            keyRight = false
             console.log("up")
             break
         case keys.arrowDown:
+            keyDown = true
+            keyUp = false
+            keyLeft = false
+            keyRight = false
             console.log("down")
             break
         case keys.arrowLeft:
+            keyLeft = true
+            keyUp = false
+            keyDown = false
+            keyRight = false
             console.log("left")
             break
         case keys.arrowRight:
+            keyRight = true
+            keyUp = false
+            keyDown = false
+            keyLeft = false
             console.log("right")
             break
         default:
             break
     }
 })
-
