860f313a1d8b266b5be49f53388f5d6d5f95669f
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2020-02-01 11:54:48 -0300

    add keyCodes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 document.getElementById("current-year").innerHTML = new Date().getFullYear();
+
+let keys = {
+    arrowUp: 38,
+    arrowDown: 40,
+    arrowLeft: 37,
+    arrowRight: 39
+}
+
+
+document.addEventListener("keydown", e => {
+
+    switch (e.keyCode) {
+        case keys.arrowUp:
+            console.log("up")
+            break
+        case keys.arrowDown:
+            console.log("down")
+            break
+        case keys.arrowLeft:
+            console.log("left")
+            break
+        case keys.arrowRight:
+            console.log("right")
+            break
+        default:
+            break
+    }
+})
